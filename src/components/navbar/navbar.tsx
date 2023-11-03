d1d484bad7aec3a3b693c347965292d2adef9990
Nikhil-Sarwara <tymnec@gmail.com>
2023-11-03 22:44:08 +0530

    Update Login to Sign In

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -27,7 +27,7 @@ export default function NavbarComponent() {
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="#">Sign In</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
