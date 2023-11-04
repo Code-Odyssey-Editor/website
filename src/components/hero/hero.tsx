0ae53c98d2b0d14b245a630343e99777e585d023
Nikhil-Sarwara <tymnec@gmail.com>
2023-11-04 15:55:13 +0530

    Add Type Animation

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,14 +1,21 @@
 import { motion } from "framer-motion";
-import {
-  Button,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-  Image,
-} from "@nextui-org/react";
+import { Image } from "@nextui-org/react";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroComponent = () => {
+  // Type Animation
+
+  const locationSequence = [
+    "Editor",
+    3000,
+    "Free (Open Source)",
+    3000,
+    "99% Uptime",
+    3000,
+    "Live Collaboration",
+    3000,
+  ];
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 100 }}
@@ -22,8 +29,21 @@ const HeroComponent = () => {
         transition={{ duration: 0.5, delay: 0.1, type: "spring" }}
         className="grid grid-cols-2 place-content-around gap-4 m-5 p-8"
       >
-        <div className="flex justify-center">
-          <div>Code Odyssey Editor</div>
+        <div className="flex flex-col justify-center">
+          <p className="text-7xl font-serif font-bold text-center text-current pt-4 leading-snug">
+            Code Odyssey
+          </p>
+          <p className="text-5xl font-mono font-bold text-center text-current pt-4 leading-snug">
+            {/* animation */}
+            <TypeAnimation
+              sequence={locationSequence}
+              speed={10}
+              repeat={Infinity}
+              wrapper="div"
+              deletionSpeed={10}
+              cursor={false}
+            />
+          </p>
         </div>
         <div className="flex justify-center mr-10">
           <Image
