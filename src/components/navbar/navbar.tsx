ea165badd601b54a3195cc800580c3a43491faec
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-11-04 10:13:04 +0530

    Revert "Patch - 0.0.2 - Navbar UI Complete"

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,51 +1,14 @@
-import React from "react";
-
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from "@nextui-org/react";
-import { AcmeLogo } from "./AcmeLogo.jsx";
-import ThemeSwitcher from "../theme_switcher/theme_toggle.tsx";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {AcmeLogo} from "./AcmeLogo.jsx";
 
 export default function NavbarComponent() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
-
   return (
-    <Navbar
-    onMenuOpenChange={setIsMenuOpen}
-      className="backdrop:blur rounded-full bg-pink-100/20 border border-gray-100/40 dark:border-gray-800/40 dark:bg-gray-800/80 w-3/4 mx-auto mt-4"
-    >
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="md:hidden text-black dark:text-white/80"
-        />
+    <Navbar position="static">
       <NavbarBrand>
         <AcmeLogo />
         <p className="font-bold text-inherit">Code Odyssey Editor</p>
       </NavbarBrand>
-      </NavbarContent>
-      <NavbarContent className="hidden lg:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="#">
             Features
@@ -62,37 +25,16 @@ export default function NavbarComponent() {
           </Link>
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent className="sm:flex gap-4" justify="end">
-        <NavbarItem>
-          <ThemeSwitcher />
-        </NavbarItem>
-      </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden md:flex">
-          <Link href="#">Sign In</Link>
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
         </NavbarItem>
-        <NavbarItem className="hidden md:flex">
+        <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu className="border dark:border-gray-700/50 w-11/12 mx-auto mt-8 rounded-[30px] backdrop:blur p-8">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
     </Navbar>
   );
 }
