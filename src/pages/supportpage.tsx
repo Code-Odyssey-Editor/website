4c7d12dd1a4bfbdb4ec112e17a20eff834b3c122
Nikhil Sarwara <tymnec@gmail.com>
2023-11-07 17:53:37 +0530

    support page fontend done

--- src/pages/supportpage.tsx
+++ src/pages/supportpage.tsx
@@ -1,11 +1,18 @@
 import FooterComponent from "../components/footer/footer";
 import CommonNavbar from "../components/navbar/common_navbar";
+import Box from "../components/support_box/box";
 
 export default function supportpage() {
   return (
     <>
       {/* Common Navbar */}
       <CommonNavbar />
+
+      {/* Background */}
+      <div className="bg-blue-100/90 dark:bg-purple-200/90 absolute -z-10 top-[6rem] right-[41rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[20rem] sm:w-[68.75rem]"></div>
+
+      {/* Support Box */}
+      <Box />
 
       {/* Footer */}
       <FooterComponent />
