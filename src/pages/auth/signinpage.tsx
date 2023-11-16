56c5861e9a7f2c678dcf89d03ab66811892fd89a
KanishqxMehta <kanishq5001.be22@chitkara.edu.in>
2023-11-16 18:56:00 +0530

    Saves

--- src/pages/auth/signinpage.tsx
+++ src/pages/auth/signinpage.tsx
@@ -10,118 +10,130 @@ import CommonNavbar from "../../components/navbar/common_navbar";
 function SignIn() {
 
   return (
-   
-    <body className="body ">
+    <div className="fixed inset-0 z-0">
+      {[...Array(100)].map((_, index) => (
+        <motion.div
+          key={index}
+          initial={{
+            opacity: 0,
+            scale: Math.random() * 0.5 + 0.2,
+            rotate: Math.random() * 360,
+            x: Math.random() * 100 - 50,
+            y: Math.random() * 100 - 50,
+          }}
+          animate={{
+            opacity: 1,
+            scale: Math.random() * 0.5 + 0.2,
+            rotate: Math.random() * 360,
+            x: Math.random() * 100 - 50,
+            y: Math.random() * 100 - 50,
+            transition: {
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: Math.random() * 3 + 1,
+              ease: 'linear',
+            },
+          }}
+          className="absolute bg-white w-1 h-1 rounded-full"
+          style={{
+            left: `${Math.random() * 100}vw`,
+            top: `${Math.random() * 100}vh`,
+          }}
+        />
+      ))}
+      <body className="bg-black">
+              {/* Common Navbar */}
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.5,
+          delay: 0.1,
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+      >
+        <CommonNavbar />
+      </motion.div>
 
-      <div className="card">
 
-        < input type="checkbox" id="chk" aria-hidden="true" name="" />
-        <div className="content">
+        <section className="flex flex-col items-center justify-center w-full flex-1 px-18 text-center min-h-screen">
 
-          <div className="front">
-            <div className="inner">
-
-              <span >Code Odyssey </span>
-              <span className="text-black"> Editor</span>
-
-              <h1 className="title">SignIn</h1>
-              <div className="flex justify-center my-2 mt-20">
-                <Link
-                  to="#"
-                  className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                >
-                  <FaGoogle />
-                </Link>
-                <Link
-                  to="#"
-                  className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                >
-                  <FaGithub />
-                </Link>
-                <Link
-                  to="#"
-                  className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                >
-                  <FaFacebook />
-                </Link>
+          <div className="bg-white rounded-2xl shadow-2xl flex w-1/2 max-w-4xl">
+            <div className="w-3/5 p-4">{/* Sign in */}
+              <div className="text-left font-bold">
+                <span className="text-green-500"> Code Odessay</span> Editor
               </div>
-              <p className="para">Just some details to get you in.!</p>
 
-              {/* <Image className=""
-                width={150}
-                alt="NextUI Fruit Image with Zoom"
-                src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-              /> */}
-              <form className="flex flex-col items-center">
-                <Input
-                  isRequired
-                  type="email"
-                  label="Email"
-                  // placeholder="Enter your Password"
-                  variant={"flat"}
-                  className="max-w-xs mt-5 overflow-hidden "
+              <div className="py-10">
+                <h2 className="text-2xl font-bold text-green-500 mb-2">Sign in</h2>
+                <p className="mb-2 text-sm">Just some details to get you in.!</p>
+                <div className="flex flex-col items-center">
+                  <div className="bg-gray-100 w-64 p-2 flex items-center mb-3 rounded-full">
+                    <MdEmail className="mr-2 ml-2" />
+                    <div className="">
+                      <Input type="email" placeholder="Enter your email" className="outline-none" />
+                    </div>
+                  </div>
 
-                />
-                <Input
-                  isRequired
-                  type="password"
-                  label="Password"
-                  // placeholder="Enter your Password"
-                  className="max-w-xs mt-3 mb-4"
-                />
-                <div className="checkboxandforgetpassword">
-                  <label >
-                    <input type="checkbox" /> Remember me
-                  </label>
 
-                  <label htmlFor="chk" aria-hidden="true" className="f_password" >
-                    Forgot password?
-                  </label>
+                  <div className="bg-gray-100 w-64 p-2 flex items-center mb-3 rounded-full">
+                    <RiLockPasswordLine className="mr-2 ml-2" />
+                    <div className="">
+                      <Input type="password" placeholder="Enter Password" className="outline-none " />
+                    </div>
+                  </div>
+                  <div className="flex justify-between w-64 mb-5 text-sm">
+                    <label><input type="checkbox" name="remember" className="mr-1" /> Remember me</label>
+                    <Link to="/forgot_p">
+                      <p>Forgot Password?</p>
+                    </Link>
+
+                  </div>
+                  <Button className=" mb-2 border-2 border-green-500 rounded-full inline-block text-green-500 hover:bg-green-500  hover:text-white hover:border-white">
+                   Signin
+                  </Button>
+
+                </div >
+                <div className="border-2 w-60 border-green-500 inline-block mb-2"></div>
+                <div className="flex justify-center ">
+                  <Link
+                    to="#"
+                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
+                  >
+                    <FaGoogle />
+                  </Link>
+                  <Link
+                    to="#"
+                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
+                  >
+                    <FaGithub />
+                  </Link>
+                  <Link
+                    to="#"
+                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
+                  >
+                    <FaFacebook />
+                  </Link>
                 </div>
-                <Button  className="font-extralight text-base mt-4 hover:bg-emerald-500 hover:text-white " >
-                  Sign In
-                </Button>
-              </form>
-
+              </div>
             </div>
 
-          </div>
-
-          <div className="back">
-            <div className="inner">
-              <span >Code Odyssey Editor</span>
-              <span className="text-black"> Editor</span>
-              <h1 className="title2">Reset Password</h1>
-              <form className="flex flex-col items-center">
-                <Input
-                  isRequired
-                  type="text"
-                  label="Username"
-                  // placeholder="Enter your Password"
-                  variant={"flat"}
-                  className="max-w-xs mt-24 "
-                />
-                <Input
-                  isRequired
-                  type="email"
-                  label="Email"
-                  // placeholder="Enter your Password"
-                  variant={"flat"}
-                  className="max-w-xs mt-3 mb-4"
-                />
-                <Button  className="font-extralight text-base mt-3 hover:bg-emerald-500 hover:text-white">
-                  Reset Password
-                </Button>
-                <label htmlFor="chk" aria-hidden="true" className="backtosigninpage" >
-                  Back to signIn page 
-                </label>
-              </form>
+            <div className="w-1/2 bg-green-600  text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">{/* Sign up */}
+              <h2 className="text-3xl font-bold mb-2 overflow-hidden ">Hello, Friend!</h2>
+              <image href="logo" />
+              <div className="border-2 w-10 border-white inline-block mb-2"></div>
+              <p className="mb-10 "> Fill up the information to start journey with us</p>
+              <Button color="success" className="border-2 border-white rounded-full inline-block text-white hover:bg-slate-50  hover:text-green-500 hover:border-green-500">
+                Signup
+              </Button>
             </div>
           </div>
         </section>
       </body>
     </div >
   );
-}
+};
 
 export default SignIn;
--- src/pages/auth/signuppage.tsx
+++ src/pages/auth/signuppage.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { Image, Input, Button, Checkbox, Link } from "@nextui-org/react";
+import CommonNavbar from "../../components/navbar/common_navbar";
+import { motion } from 'framer-motion';
+
 
 const Signup: React.FC = () => {
   return (
     <div className="bg-gray-300 min-h-screen flex items-center justify-center">
-      <div className="bg-gray-200 flex rounded-2xl shadow-lg max-w-3xl p-5 lg:flex-row flex-col h-full">
-        <div className="w-1/2 mt-5 pr-1 pl-2 ">
-          <Image
-            isZoomed
-            alt="NextUI Fruit Image with Zoom"
-            src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
-            className="object-cover w-full h-full"
-          />
-        </div>
-
-      <motion.div
+            {/* Common Navbar */}
+            <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -28,122 +22,60 @@ const Signup: React.FC = () => {
         <CommonNavbar />
       </motion.div>
 
-      <body className="dark:bg-black">
-        <section className="flex flex-col items-center justify-center w-full flex-1 px-18 text-center min-h-screen">
-          <div className="bg-white rounded-2xl shadow-2xl flex w-4/6 max-w-4xl z-10 dark:bg-slate-950">
-            <div className="hidden md:flex flex-col justify-center items-center w-full bg-green-600 dark:bg-green-700  text-white rounded-l-2xl py-36 px-12">
-              {/* Sign up */}
-              <h2 className="text-3xl font-bold mb-2 overflow-hidden ">
-                Hello, Friend!
-              </h2>
-              <h2 className="text-xl font-bold mb-2 overflow-hidden ">
-                WELCOME
-              </h2>
-              <image href="logo" />
-              <div className="border-2 w-10 border-white inline-block mb-2"></div>
-              <p className="mb-5">
-                Fill up the information to start journey with us
-              </p>
-              <Link to="/website/sign-in">
+      <div className="bg-gray-200 flex rounded-2xl shadow-lg max-w-3xl p-5 lg:flex-row flex-col h-full">
+        <div className="w-1/2 mt-5 pr-1 pl-2 ">
+          <Image
+            isZoomed
+            alt="NextUI Fruit Image with Zoom"
+            src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        <div className="rounded-tl-3xl bg-gradient-to-r from-gray-700 via-white-500 to-slate-800 w-full lg:w-1/2 p-6">
+          <div className="">
+            <h2 className="text-white font-bold text-2xl">Welcome</h2>
+            <p className="text-white text-xs mt-4">
+              We are glad to see you back with us
+            </p>
+            <form>
+              <Input
+                type="email"
+                id="email"
+                label="Email"
+                defaultValue=""
+                className="max-w-[290px] mt-5"
+              />
+
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                defaultValue=""
+                className="max-w-[290px] mt-5"
+              />
+
+              <div className="flex items-center mt-6">
+                <Checkbox defaultSelected>Remember me</Checkbox>
+                <Link color="primary" href="/forgot-password" className="ml-6">
+                  Forgot Password?
+                </Link>
+              </div>
+
+              <div className="pl-28">
                 <Button
-                  color="success"
-                  className="border-2 border-white rounded-full inline-block text-white hover:bg-slate-50 hover:text-green-500 hover:border-green-500"
+                  className="max-w-[290px] mt-6 max-h-[30px]"
+                  color="primary"
                 >
                   Sign In
                 </Button>
-              </Link>
-            </div>
-
-            <div className="w-full p-4">
-              {/* Sign in */}
-              <div className="text-left font-bold">
-                Welcome to <span className="text-green-600">Code Odessay</span>{" "}
-                Editor
               </div>
-
-              <div className="py-10">
-                <h2 className="text-2xl font-bold text-green-500 mb-2">
-                  Sign up
-                </h2>
-                <p className="mb-2 text-sm">
-                  Just some details to get you in.!
-                </p>
-                <div className="flex flex-col items-center gap-3">
-                  <div className="bg-gray-100 dark:bg-slate-900 w-64 p-2 flex items-center mb-1 rounded-full">
-                    <MdEmail className="mr-2 ml-2" />
-                    <div className="">
-                      <Input
-                        type="email"
-                        variant="underlined"
-                        label="Email"
-                        placeholder="Enter your email"
-                        className="outline-none"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="bg-gray-100 dark:bg-slate-900 w-64 p-2 flex items-center mb-1 rounded-full">
-                    <RiLockPasswordLine className="mr-2 ml-2" />
-                    <div className="">
-                      <Input
-                        type="password"
-                        variant="underlined"
-                        label="Password"
-                        placeholder="Enter Password"
-                        className="outline-none "
-                      />
-                    </div>
-                  </div>
-
-                  <div className="bg-gray-100 dark:bg-slate-900 w-64 p-2 flex items-center mb-1 rounded-full">
-                    <RiLockPasswordLine className="mr-2 ml-2" />
-                    <div className="">
-                      <Input
-                        type="password"
-                        variant="underlined"
-                        label="Confirm Password"
-                        placeholder="Confirm Password"
-                        className="outline-none "
-                      />
-                    </div>
-                  </div>
-
-                  <label className="m-1">
-                    <input type="checkbox" name="remember" className="" />{" "}
-                    Remember me
-                  </label>
-                  <Button className=" mb-2 border-2 border-green-500 rounded-full inline-block text-green-500 hover:bg-green-500  hover:text-white hover:border-white">
-                    Sign Up
-                  </Button>
-                </div>
-                <div className="border-2 w-60 border-green-500 inline-block mb-2"></div>
-                <div className="flex justify-center ">
-                  <Link
-                    to="#"
-                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                  >
-                    <FaGoogle />
-                  </Link>
-                  <Link
-                    to="#"
-                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                  >
-                    <FaGithub />
-                  </Link>
-                  <Link
-                    to="#"
-                    className="border-2 border-gray-200 rounded-full py-2 px-4 mx-1 text-gray-600 hover:bg-gray-200 hover:text-gray-700 transition ease-in-out duration-300"
-                  >
-                    <FaFacebook />
-                  </Link>
-                </div>
-              </div>
-            </div>
+            </form>
           </div>
-        </section>
-      </body>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Signup;
